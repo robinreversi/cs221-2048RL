7dcaea1e054a0d40e88969254b065ac4ed95e693
Jiaqi (Alex) Wang <jwang98@stanford.edu>
2017-11-17 15:00:22 -0800

    multiple boards

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
-import collections, random
+import collections, random, operator
 import gamestate
 import player
 
 def play2048():
-    game = gamestate.Game_2048()
+    games = [gamestate.Game_2048() for _ in range(4)]
     def evalFn(currentGameState):
+        if currentGameState.isEnd():
+            return float('-inf')
         weights = [range(i,i+4) for i in range(1,5)]
         sum = 0.0
         for i in range(4):
@@ -12,13 +14,29 @@ def play2048():
                 sum += weights[i][j] * currentGameState.board[i,j]
         return currentGameState.getScore() + sum
     agent = player.Player(2, evalFn)
-    while not game.isEnd():
-        action = agent.getAction(game.copy())
-        print "Action: " + str(action)
-        game.swipe(action)
-        game.placeRandomTile()
-        game.printScore()
-        game.printBoard()
+    done = False
+    while not done:
+        values = collections.defaultdict(float)
+        count = collections.defaultdict(float)
+        for game in games:
+            action,vals = agent.getAction(game.copy())
+            print "Action: " + str(action)
+            for move,score in vals:
+                values[move] += score
+                count[move] += 1
+
+        for key in values:
+            values[move] /= count[move]
+        action = max(values.iteritems(), key=operator.itemgetter(1))[0]
+        total = 0.0
+        for game in games:
+            game.swipe(action)
+            game.placeRandomTile()
+            total += game.getScore()
+            game.printBoard()
+            if game.isEnd():
+                done = True
+        print('Current score is ', total)
 
 
 play2048()
