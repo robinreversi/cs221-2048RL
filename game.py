baf79537d7c8ea5b92ff54d6434ce33a69730930
Vince <akranga@stanford.edu>
2017-11-17 03:39:11 -0800

    added multiple boards to game.py

--- game.py
+++ game.py
@@ -3,22 +3,31 @@ import gamestate
 import player
 
 def play2048():
-    game = gamestate.Game_2048()
+    game = gamestate.Multi_Game_2048()
+
     def evalFn(currentGameState):
-        weights = [[7,6,5,4],[6,5,4,3],[5,4,3,2],[4,3,2,1],[3,2,1,0]]
+        weights = [[7,6,5,4],[6,5,4,3],[5,4,3,2],[4,3,2,1]]
         sum = 0.0
         for i in range(4):
             for j in range(4):
                 sum += weights[i][j] * currentGameState.board[i,j]
         return currentGameState.getScore() + sum
     agent = player.Player(2, evalFn)
+
     while not game.isEnd():
+        #TODO this line
         action = agent.getAction(game.copy())
+
         print "Action: " + str(action)
         game.swipe(action)
-        game.placeRandomTile()
-        game.printScore()
-        game.printBoard()
+        game.updateBoard()
 
 
-play2048()
+####################################################
+
+def main():
+    play2048()
+
+
+if __name__ == '__main__':
+    main()
