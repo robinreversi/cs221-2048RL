c56695c92ccc3cf6ef12d53a20f744864c28eb28
robinreversi <robincheong@gmail.com>
2017-11-30 22:28:48 -0800

    Rough version of Q-learning working

--- q_learning.py
+++ q_learning.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from operator import itemgetter
 
 LOSS_BATCH_SIZE = 20
-EPSILON = .001
+EPSILON = .000001
 NUM_FEATURES = 3
+TEST_NUM = 10
 
 def approximate(obs, action):
     '''
@@ -14,51 +16,78 @@ def approximate(obs, action):
     '''
     return np.array([1, sum(obs), max(obs)])
     
-def q_learning(f, env, step_size, discount):
+def run_game(w, env, train, num_games, f, discount, step_size):
+    game_over = False
     action_space = range(env.action_space.n)
+    cur = env.reset()
+    total_loss = 0
+    while not game_over:
+        print "===================================="
+        print "-----------------BEFORE---------------\n"
+        env.render()
+        # pick the action that maximizes Q_opt(cur, a) = w . f(a)
+        # ROUGH PSEUDO-CODE, UNTESTED
+        action = None
+        features = dict((a, f(cur, a)) for a in action_space)
+        potentials = dict((a, w.dot(features[a])) for a in features)
+        if(random.random() < 1.0 / (num_games + .1) and train):
+            print "\nRANDOM ACTION TAKEN\n"
+            action = env.action_space.sample()
+        else:
+            action = max(potentials.iteritems(), key=itemgetter(1))[0] 
+        Q_opt = potentials[action]
+        phi = features[action]
+        cur, reward, game_over, info = env.step(action)
+
+        print "\n-----------AFTER ACTION: " + str(action) + "------------\n"
+        env.render()
+        print "====================================="
+
+        V_opt = max([w.dot(f(cur, a)) for a in action_space])
+
+        loss = Q_opt - (reward + discount * V_opt)
+        total_loss += loss
+        # learn
+        if(train):
+            w = w - step_size * loss * phi
+        print "\n WEIGHTS: " + str(w) + "\n"
+
+    return total_loss, env.score
+
+def q_learning(f, env, step_size, discount):
+    print "START"
     has_converged = False
+    
     env.seed()
     init = env.reset()
-    w = np.zeros(NUM_FEATURES)
-    avg_losses = []
 
+    w = np.zeros(NUM_FEATURES)
+    avg_losses = np.array([])
+    scores = []
+    game_count = 0
     while not has_converged:
         env.seed()
         cur = env.reset()
 
-        game_over = False
-        
-        losses = []
-        i = 0
-        while not game_over and not has_converged:
-            
-            print "BEFORE:"
-            env.render()
-            # pick the action that maximizes Q_opt(cur, a) = w . f(a)
-            # ROUGH PSEUDO-CODE, UNTESTED
-            features = dict((a, f(cur, a)) for a in action_space)
-            potentials = dict((a, w.dot(features[a])) for a in features)
-            action = max(potentials.iteritems(), key=itemgetter(1))[0]
-            Q_opt = potentials[action]
-            phi = features[action]
-            print "ACTION" + str(action)
-            cur, reward, game_over, info = env.step(action)
-            print "AFTER"
-            env.render()
-            
-            loss = Q_opt - (reward + discount * max([w.dot(f(cur, a)) for a in action_space]))
-            losses.append(loss)
-            
-            # learn
-            w = w - step_size * loss * phi
-            
-            if(i % LOSS_BATCH_SIZE == 0):
-                avg_loss = sum(losses) / float(LOSS_BATCH_SIZE)
-                avg_losses.append(avg_loss)
+        run_game(w, env, True, game_count, f, discount, step_size)
+
+        game_count += 1
+
+        if(game_count % 15 == 0):
+            avg_loss = 0 
+            avg_score = 0
+            for i in range(TEST_NUM):
+                env.seed()
+                init = env.reset()
+                loss, score = run_game(w, env, False, 1, f, discount, step_size)
+                avg_loss += loss
+                avg_score += score
+
+            if(avg_loss / float(TEST_NUM) < EPSILON):
+                has_converged = True
                 print avg_loss
-                if(avg_loss < EPSILON):
-                    has_converged = True
-            
-    print avg_losses
-    plt.plot(avg_losses)
+                print avg_score
+    
+    return w
+    #plt.show()
     
