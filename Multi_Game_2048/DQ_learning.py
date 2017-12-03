a9a44de86bde23e30e4ec2347a596279b6b7c479
robinreversi <robincheong@gmail.com>
2017-12-02 16:22:53 -0800

    Testing

--- Multi_Game_2048/DQ_learning.py
+++ Multi_Game_2048/DQ_learning.py
@@ -2,7 +2,8 @@ import gym
 import numpy as np
 import random
 import tensorflow as tf
-import matplotlib.pyplot as plt
+import Multi_Game_2048.Multi_Game_2048_env
+
 
 DISCOUNT = .9
 NUM_GAMES = 100000
@@ -13,20 +14,17 @@ env = gym.make('Multi2048-v0')
 num_boards = env.n
 
 board_vals = tf.placeholder(shape=[1, 16 * num_boards],dtype=tf.float32)
-print inputs1
+print(inputs1)
 W = tf.Variable(tf.random_uniform([16 * num_boards, 4], 0, 0.01))
-print W
+print(W)
 q_values = tf.matmul(inputs1, W)
-print Qout
-action = tf.argmax(Qout,1)
+print(q_values)
+action = tf.argmax(q_values,1)
 
 nextQ = tf.placeholder(shape=[1,4],dtype=tf.float32)
-loss = tf.reduce_sum(tf.square(nextQ - Qout))
-trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+loss = tf.reduce_sum(tf.square(nextQ - q_values))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 updateModel = trainer.minimize(loss)
-
-return env
-
 
 
 with tf.Session() as sess:
@@ -38,18 +36,24 @@ with tf.Session() as sess:
         done = False
         env.render()
         while not done:
-            a, q_values = sess.run([action, Qout], feed_dict={board_vals:obs})
-            print a
+            a, init_q_values = sess.run([action, q_values], feed_dict={board_vals:obs})
+            print(a)
             if(random.random() < EPSILON):
                 a[0] = env.action_space.sample()
 
-            obs, reward, done, info = env.step(a[0])
+            new_obs, reward, done, info = env.step(a[0])
 
-            new_q_values = sess.run(Qout, feed_dict={board_vals:obs})
+            new_q_values = sess.run(Qout, feed_dict={board_vals:new_obs})
 
             # V of the new state = max of the q values
-            maxQ1 = np.max(Q1)
+            max_new_q = np.max(new_q_values)
+            targetQ = init_q_values
+            targetQ[0, a[0]] = r + DISCOUNT * max_new_q
 
-            _, W1 = sess.run([updateModel, W], feed_dict={board_vals: , nextQ: new_q_values})
+            _, W1 = sess.run([updateModel, W], feed_dict={board_vals: obs, nextQ: targetQ})
+
+            obs = new_obs
 
         env.render()
+
+        EPSILON = 1./((_/50) + 10)
